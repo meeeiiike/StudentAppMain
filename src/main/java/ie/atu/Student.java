abdfe32fbf729e26f3f11d5ca6d9ea088ff63158
meeeiiike <g00407095@atu.ie>
2022-10-24 11:30:24 +0100

    prompt user for name

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -1,4 +1,12 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Student {
+    public static void main(String[] args) {
+        System.out.println("Please enter name:  ");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("you entered : " + name);
+    }
 }
