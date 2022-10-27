97d12c8ac6d1dec82567fed5baf6816c08d08f0f
meeeiiike <g00407095@atu.ie>
2022-10-28 00:59:35 +0100

    FINAL PRODUCT WHOOP WHOOP

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -21,29 +21,39 @@ public class Student {
         userDetails student2 = new userDetails();
         System.out.println("Please enter second name:  \n");
         String name2 = input.nextLine();
-        student2.setName(name);
+        student2.setName(name2);
 
         System.out.println("Please enter second email:  \n");
         String email2 = input.nextLine();
-        student2.setEmail(email);
+        student2.setEmail(email2);
 
         System.out.println("Please enter second course:  \n");
         String course2 = input.nextLine();
-        student2.setCourse(course);
+        student2.setCourse(course2);
 
         userDetails student3 = new userDetails();
         System.out.println("Please enter second name:  \n");
         String name3 = input.nextLine();
-        student3.setName(name);
+        student3.setName(name3);
 
         System.out.println("Please enter second email:  \n");
         String email3 = input.nextLine();
-        student3.setEmail(email);
+        student3.setEmail(email3);
 
         System.out.println("Please enter second course:  \n");
         String course3 = input.nextLine();
-        student3.setCourse(course);
+        student3.setCourse(course3);
 
+        System.out.println("Student No.1: " + student.getName());
+        System.out.println("Email: " + student.getEmail());
+        System.out.println("Course: " + student.getCourse());
 
+        System.out.println("Student No.2: " + student2.getName());
+        System.out.println("Email: " + student2.getEmail());
+        System.out.println("Course: " + student2.getCourse());
+
+        System.out.println("Student No.3: " + student3.getName());
+        System.out.println("Email: " + student3.getEmail());
+        System.out.println("Course: " + student3.getCourse());
     }
 }
