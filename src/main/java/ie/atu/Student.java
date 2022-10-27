c19d0f3a66a77c9b77ae19ba01f2e45322d04032
meeeiiike <g00407095@atu.ie>
2022-10-28 00:53:48 +0100

    INSTANCES ADDED

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -7,26 +7,43 @@ public class Student {
         System.out.println("Please enter name:  \n");
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
-        System.out.println("you entered : " + name);
+        userDetails student = new userDetails();
+        student.setName(name);
 
         System.out.println("Please enter email:  \n");
         String email = input.nextLine();
-        System.out.println("you entered : " + email);
+        student.setEmail(email);
 
         System.out.println("Please enter course:  \n");
         String course = input.nextLine();
-        System.out.println("you entered : " + course);
+        student.setCourse(course);
 
+        userDetails student2 = new userDetails();
         System.out.println("Please enter second name:  \n");
         String name2 = input.nextLine();
-        System.out.println("you entered : " + name2);
+        student2.setName(name);
 
         System.out.println("Please enter second email:  \n");
         String email2 = input.nextLine();
-        System.out.println("you entered : " + email2);
+        student2.setEmail(email);
 
         System.out.println("Please enter second course:  \n");
         String course2 = input.nextLine();
-        System.out.println("you entered : " + course2);
+        student2.setCourse(course);
+
+        userDetails student3 = new userDetails();
+        System.out.println("Please enter second name:  \n");
+        String name3 = input.nextLine();
+        student3.setName(name);
+
+        System.out.println("Please enter second email:  \n");
+        String email3 = input.nextLine();
+        student3.setEmail(email);
+
+        System.out.println("Please enter second course:  \n");
+        String course3 = input.nextLine();
+        student3.setCourse(course);
+
+
     }
 }
