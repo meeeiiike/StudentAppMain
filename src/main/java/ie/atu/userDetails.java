4c8b44fc50efce5e6129d31015b33c2535ad5353
meeeiiike <g00407095@atu.ie>
2022-10-28 01:01:07 +0100

    FINAL PRODUCT WHOOP WHOOP

--- src/main/java/ie/atu/userDetails.java
+++ src/main/java/ie/atu/userDetails.java
@@ -5,27 +5,38 @@ public class userDetails {
     private String email;
     private String course;
 
+    public userDetails() {
+        this.name = "";
+        this.email = "";
+        this.course = "";
+    }
     public String getName() {
-        return name;
+
+        return this.name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getEmail() {
-        return email;
+
+        return this.email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
     public String getCourse() {
-        return course;
+
+        return this.course;
     }
 
     public void setCourse(String course) {
+
         this.course = course;
     }
 }
